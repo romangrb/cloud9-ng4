de5d1604eddc908965d3779939bd5fb878dc5a03
Roman <grabarroman@gmail.com>
2017-08-15 20:18:28 +0000

    add basic auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,59 @@
 import { BrowserModule }         from '@angular/platform-browser';
 import { NgModule }              from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
+import { HttpModule }            from '@angular/http';
+import { FormsModule }           from '@angular/forms';
+import { MockBackend, 
+         MockConnection }        from '@angular/http/testing';
 
 import { AppComponent }          from './app/components/app';
+import { routing }               from './app.routing';
+
 import { PageAComponent }        from './algorithm/components/a';
 import { PageBComponent }        from './algorithm/components/b';
 import { PageNotFoundComponent } from './app/components/not-found';
 
+import { fakeBackendProvider }   from './_helpers/index';
+import { BaseRequestOptions }    from '@angular/http';
 
-const appRoutes: Routes = [
-  
-  { path: '',   component: AppComponent },
-  { path: 'page-a', component: PageAComponent },
-  { path: 'page-b', component: PageBComponent },
-  { path: '**', component: PageNotFoundComponent }
-  
-];
+import { AlertComponent }        from './alert/directives/alert';
+import { AuthGuard }             from './_guards/index';
+import { AlertService }          from './alert/services/index';
+import { AuthenticationService } from './authentication/index';
+import { UserService }           from './user/services/user';
+
+import { HomeComponent }         from './home/index';
+import { LoginComponent }        from './login/index';
+import { RegisterComponent }     from './register/components/register';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    AlertComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
     PageAComponent,
     PageBComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true }
-    )
+    FormsModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    // providers used to create fake backend
+    fakeBackendProvider,
+    MockBackend,
+    BaseRequestOptions
+  ],
   bootstrap: [AppComponent]
 })
 
---
+++ src/app.routing.ts
@@ -0,0 +1,23 @@
+import { Routes, RouterModule }  from '@angular/router';
+
+import { HomeComponent }         from './home/index';
+import { LoginComponent }        from './login/index';
+import { RegisterComponent }     from './register/index';
+import { AuthGuard }             from './_guards/index';
+import { PageAComponent }        from './algorithm/components/a';
+import { PageBComponent }        from './algorithm/components/b';
+import { PageNotFoundComponent } from './app/components/not-found';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    { path: 'page-a', component: PageAComponent },
+    { path: 'page-b', component: PageBComponent },
+    
+    { path: '**', component: PageNotFoundComponent }
+
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
